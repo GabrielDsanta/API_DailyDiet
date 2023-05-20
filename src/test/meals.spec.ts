ca30e9dcfd65abfeb53347bf88c479895b15aa24
GabrielDSanta <eloviveiros@hotmail.com>
2023-05-20 14:29:03 -0300

    Projeto finalizado

--- src/test/meals.spec.ts
+++ src/test/meals.spec.ts
@@ -46,6 +46,42 @@ describe('Meals routes', () => {
             .expect(201)
     })
 
+    it('must be possible to edit a meal, being able to change all data', async () => {
+        const createUserRespose = await request(server.server)
+            .post('/users')
+            .send({
+                name: "Arthur Gabriel"
+            })
+
+        const cookies = createUserRespose.get('Set-Cookie')
+
+        await request(server.server)
+            .post('/meals')
+            .send({
+                name: "Refeição Teste",
+                description: "Negocio aqui",
+                time: "15:35",
+                date: "27/05/2023",
+                isInDiet: false
+            })
+            .set('Cookie', cookies)
+
+        const findMeal = await request(server.server)
+            .get('/meal')
+
+        await request(server.server)
+            .put(`/meals/${findMeal.body.id}`)
+            .set('Cookie', cookies)
+            .send({
+                name: "Refeição Teste Alterada",
+                description: "Negocio aqui Alterado",
+                time: "15:55",
+                date: "21/06/2023",
+                isInDiet: true
+            })
+            .expect(201)
+    })
+
     it('It should be possible to list all meals of a user', async () => {
         const createUserRespose = await request(server.server)
             .post('/users')
@@ -70,7 +106,6 @@ describe('Meals routes', () => {
             .get('/meals')
             .set('Cookie', cookies)
             .expect(200)
-        console.log(findMeal.body.meals)
 
         expect(findMeal.body.meals).toEqual([
             expect.objectContaining({
@@ -79,6 +114,47 @@ describe('Meals routes', () => {
                 time: "15:35",
                 date: "27/05/2023",
                 isInDiet: 1,
+            })
+        ])
+    })
+
+    it('It should be possible to visualize a single meal', async () => {
+        const createUserRespose = await request(server.server)
+            .post('/users')
+            .send({
+                name: "Arthur"
+            })
+
+        const cookies = createUserRespose.get('Set-Cookie')
+
+        await request(server.server)
+            .post('/meals')
+            .send({
+                name: "Maça",
+                description: "Refeição saudável",
+                time: "15:35",
+                date: "27/05/2023",
+                isInDiet: true
+            })
+            .set('Cookie', cookies)
+
+        const findMeal = await request(server.server)
+            .get('/meals')
+            .set('Cookie', cookies)
+            .expect(200)
+
+        const findMealByIdResponse = await request(server.server)
+            .get(`/meals/${findMeal.body.meals[0].id}`)
+            .set('Cookie', cookies)
+            .expect(200)
+
+        expect(findMealByIdResponse.body.meal).toEqual([
+            expect.objectContaining({
+                name: "Maça",
+                description: "Refeição saudável",
+                time: "15:35",
+                date: "27/05/2023",
+                isInDiet: 1
             })
         ])
     })
